1736b3cd5d314fa51f7b9ce7f11726340f81d3cd
Marcelo <marcelosen4@gmail.com>
2021-01-14 17:13:17 -0300

    agregado funciones session y connect-flash

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 const app = express();
 
@@ -9,6 +11,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+// con esto tenemos una sesion en el servidor, un espacio en memoria que se puede compartir entre multiples paginas
+app.use(session({
+    secret: 'secretkey',
+    resave: false,
+    saveUninitialized: false
+}))
+// modulo de flash para poder compartir mensajes entre diferentes vistas
+app.use(flash());
+
+// global variable
+// middleware para definir variables globales y poder mostrarlas en cualquier vista
+app.use((req, res, next) => {
+    // para pasar este dato a todas las vistas, en express existe una propiedad llamada app.locals.(algun nombre)
+    // por ejemplo creamos la variable llamada message, el cual tiene el valor success
+    // ahora esta variable message puede ser accedida desde cualquier vista
+    app.locals.message = req.flash('success');
+    // una vez que hemos guardado ese valor le decimos que continue con next
+    next();
+});
 
 // routes
 app.use(require('./routes/index'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,48 @@ const router = Router();
 
 router.get('/', (req, res) => {
     res.render('index');
+});
+
+router.post('/register', (req, res) => {
+    console.log(req.body);
+    /* forma de escribirlo usando el modulo session */
+    // para enviar un mensaje entre paginas, como por ejemplo una variable o req.body
+    // req.session.my_variable = 'Hello World';
+    // req.session.user_data = req.body;
+
+    // usando el modulo flash
+    // lo mismo que habiamos hecho antes, solo que ahora desde una funcion
+    // req.flash('user', req.body);
+
+    req.flash('success', 'Now you are registered');
+    res.redirect('/profile');
+
+    // res.redirect('/products');
+});
+
+router.get('/profile', (req, res) => {
+    /* forma de escribirlo usando el modulo session */
+    // para mostrar la variable de la sesion
+    // const user = req.session.user_data;
+    // una vez pasado el mensaje se elimina porque es temporal
+    // delete req.session.user_data;
+    // como está guardada en una variable se lo podemos pasar al render
+    // de esta forma estamos enviado datos entre una vista a otra utilizando las sesiones
+
+    // en una constante user guardamos lo que desde req.flash trae en su variable user
+    // const user = req.flash('user');
+    // como flash devuelve un arreglo le pasamos el indice cero
+    // const message = req.flash('success')[0];
+    // para luego mostrarlo en la vista
+    // res.render('profile', {
+    //     message
+    // });
+
+    res.render('profile');
+});
+
+router.get('/products', (req, res) => {
+    res.render('products');
 })
 
 module.exports = router;
